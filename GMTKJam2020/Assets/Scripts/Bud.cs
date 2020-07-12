fc059e226c211f05ef3dbb1d1d72fee31397e9fa
Zachary Sheppard <the_ds_guy@yahoo.com>
2020-07-12 00:38:14 -0500

    Adding procedural animation to bud

--- GMTKJam2020/Assets/Scripts/Bud.cs
+++ GMTKJam2020/Assets/Scripts/Bud.cs
@@ -1,8 +1,12 @@
-﻿using UnityEngine;
+﻿using UnityAtoms.BaseAtoms;
+using UnityEngine;
 using UnityEngine.AI;
 
 public class Bud : MonoBehaviour, IClickable
 {
+
+    [SerializeField]
+    private Transform m_visualsParent = null;
 
     void IClickable._HandleClick()
     {
@@ -12,9 +16,40 @@ public class Bud : MonoBehaviour, IClickable
     [SerializeField]
     private NavMeshAgent m_agent = null;
 
+    [SerializeField]
+    private FloatReference m_wobbleAmount = null;
+
+    [SerializeField]
+    private FloatReference m_wobbleSpeed = null;
+
     public void SetPosition(Vector3 position)
     {
         m_agent.SetDestination(position);
+    }
+
+    private void Update()
+    {
+        if (m_agent.isStopped)
+        {
+            return;
+        }
+
+        var angles = m_visualsParent.transform.localRotation;
+        var normalizedDesiredVelocity = m_agent.desiredVelocity.normalized;
+
+        Quaternion newRot = Quaternion.identity;
+        if (normalizedDesiredVelocity.magnitude > 0f)
+        {
+            var eulerAngles = angles.eulerAngles;
+            eulerAngles.z = Mathf.Sin(Time.time * m_wobbleSpeed) * m_wobbleAmount;
+            newRot = Quaternion.Euler(eulerAngles * normalizedDesiredVelocity.magnitude);
+        }
+        else
+        {
+            newRot = Quaternion.Slerp(m_visualsParent.transform.localRotation, newRot, Time.deltaTime * 10f);
+        }
+
+        m_visualsParent.transform.localRotation = newRot;
     }
 
 }
